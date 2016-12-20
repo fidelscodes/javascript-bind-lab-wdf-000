0471a3ada0314be0431d862d1b77164abbdfddfc
fidelscodes <fidelscodes@gmail.com>
2016-12-20 18:45:16 -0500

    Done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-const app = "I don't do much.";
+let digitalClock = {
+  time: Math.round(Date.now() / 1000),
+  startTicking: function() {
+    setInterval(this.time++, 1000); // Why does this work without using bind or arrow functions?
+  }
+};
+
+function censor(wordToCensor, stringToSanitize) {
+  // this wouldn't work for creating a regular expression
+  // most likely due to the wordToCensor variable, hence the need for new RegExp()
+  // let regex = /wordToCensor/gi;
+  let regex = new RegExp(wordToCensor, 'gi'); // replace globally, ignoring case
+  return stringToSanitize.replace(regex, 'BLEEP');
+}
+
+let violenceCensor = censor.bind(null, 'violence');
+let drugsCensor = censor.bind(null, 'drugs');
